708af0539da8d29b4ba028dd2cd0c55dd60035f7
Ingrid Marcal <marcal.delaine@gmail.com>
2025-03-30 17:22:58 +1100

    Add search recipe input

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,15 +1,28 @@
 import RecipeCard from "../components/RecipeCard.jsx";
-import {Grid} from "@mui/material";
+import {FormGroup, Grid, TextField} from "@mui/material";
 import BasePageLayout from "../BasePageLayout.jsx";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
+import Button from "@mui/material/Button";
 
 
 const Homepage = () => {
+
+    const handleFindRecipeByName = (e) => {
+        e.preventDefault();
+        alert("sent")
+    }
+
     return (
         <BasePageLayout>
             <Typography variant="h2">Welcome</Typography>
-            <Typography variant="h6">Choose a recipe</Typography>
+            <Typography variant="h6">&nbsp;</Typography>
+            <form onSubmit={handleFindRecipeByName} >
+                <FormGroup row>
+                    <TextField sx={{ width: 1/3 }} id="outlined-basic" label="Find a recipe by name" variant="outlined" size="small" />
+                    <Button type="submit" variant="contained" size="medium" disableElevation>Find recipe</Button>
+                </FormGroup>
+            </form>
             <Grid container spacing={2} my={2}>
                 <Grid>
                     <RecipeCard isFavourite={false}/>
