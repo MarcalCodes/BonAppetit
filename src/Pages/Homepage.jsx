0348f5a1465746ec24f9671a044cab59b3343e25
Ingrid Marcal <marcal.delaine@gmail.com>
2025-03-30 18:55:08 +1100

    Show when no recipe found

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -9,7 +9,7 @@ import TheMeanDBClient from "../Clients/TheMeanDBClient.js";
 
 
 const Homepage = () => {
-    const [userSearchedRecipes, setUserSearchedRecipes] = useState([])
+    const [userSearchedRecipes, setUserSearchedRecipes] = useState(undefined)
 
     const handleFindRecipeByName = async (e) => {
         e.preventDefault();
@@ -22,10 +22,11 @@ const Homepage = () => {
         <BasePageLayout>
             <Typography variant="h2">Welcome</Typography>
             <Typography variant="h6">&nbsp;</Typography>
-            <Box sx={{ mb: 6 }}>
+            <Box sx={{mb: 6}}>
                 <form onSubmit={handleFindRecipeByName}>
                     <FormGroup row>
-                        <TextField sx={{width: 1 / 3}} id="outlined-basic" label="Find a recipe by name" variant="outlined"
+                        <TextField sx={{width: 1 / 3}} id="outlined-basic" label="Find a recipe by name"
+                                   variant="outlined"
                                    size="small" name="userFilter"/>
                         <Button type="submit" variant="contained" size="medium" disableElevation>Find recipe</Button>
                     </FormGroup>
@@ -33,18 +34,22 @@ const Homepage = () => {
             </Box>
             <Grid container spacing={2} my={2} gap={4}>
                 {
-                    userSearchedRecipes.map((recipe, index) =>
-                        <Grid key={index}>
-                            <RecipeCard
-                                key={index}
-                                id={recipe.idMeal}
-                                name={recipe.strMeal}
-                                image={recipe.strMealThumb}
-                                category={recipe.strCategory}
-                                area={recipe.strArea}
-                                isFavourite={false}/>
-                        </Grid>
-                    )
+                    userSearchedRecipes === undefined
+                        ? <Box/> // user just arrived on the page and didn't search yet for anything
+                        : userSearchedRecipes.length === 0
+                            ? <Typography>No Recipe found</Typography>
+                            : userSearchedRecipes.map((recipe, index) =>
+                                <Grid key={index}>
+                                    <RecipeCard
+                                        key={index}
+                                        id={recipe.idMeal}
+                                        name={recipe.strMeal}
+                                        image={recipe.strMealThumb}
+                                        category={recipe.strCategory}
+                                        area={recipe.strArea}
+                                        isFavourite={false}/>
+                                </Grid>
+                            )
                 }
             </Grid>
         </BasePageLayout>
