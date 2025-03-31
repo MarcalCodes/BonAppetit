a81a382a14b53ccf808613cd8a380377d52f920e
Ingrid Marcal <marcal.delaine@gmail.com>
2025-03-31 17:38:47 +1100

    add UserProvider and LoginForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import AppRoutes from "./Routes/AppRoutes.jsx";
+import {UserProvider} from "./Context/userContext.jsx";
 
 function App() {
 
     return (
-        <>
+        <UserProvider>
             <AppRoutes/>
-        </>
+        </UserProvider>
     )
 }
 
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,79 @@
+import {useState} from "react";
+import {useUserContext} from "../Context/userContext.jsx";
+
+const LoginForm = () => {
+    // input state values always need to be strings - empty initially
+    const [userEmail, setUserEmail] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+    const [submitResult, setSubmitResult] = useState("");
+    // destructure the context values passed via UserProvider
+    const {currentUser, handleUpdateUser} = useUserContext();
+    // destructure theme context
+    // to use this context in a component, first employ useContext hook
+    // const { theme } = useContext(MyThemeContext);
+    // alternative using the useContext hook directly, either works
+    //const {currentUser, handleUpdateUser} = useContext(UserContext);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (userPassword === "pass" && userEmail === "ingrid") {
+            setSubmitResult("Successful login.");
+            handleUpdateUser({email: userEmail}); // context function
+        } else {
+            setSubmitResult("User not found");
+        }
+    };
+
+    /* if the user is already logged in, show msg instead of form */
+    if (currentUser)
+        return (
+            <>
+                <p>Welcome {currentUser.email}!</p>
+                <button onClick={() => handleUpdateUser(undefined)}>Log Out</button>
+            </>
+        );
+    return (
+        <>
+            <div>
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <div className="formRow">
+                            <label>
+                                Email Address:
+                                <br/>
+                                {/* Controlled form element needs both value and onChange.
+                                onChange handler uses event param e to access target value.
+                                   Whenever user types, new value is stored in state. */}
+                                <input
+                                    type="email"
+                                    value={userEmail}
+                                    name="userEmail"
+                                    onChange={(e) => setUserEmail(e.target.value)}
+                                />
+                            </label>
+                        </div>
+                        <div className="formRow">
+                            <label>
+                                Password:
+                                <br/>
+                                <input
+                                    type="password"
+                                    value={userPassword}
+                                    name="password"
+                                    onChange={(e) => setUserPassword(e.target.value)}
+                                />
+                            </label>
+                        </div>
+                        {/* same form code as previously, BUT now includes
+                        <form> and <button> */}
+                        <br/>
+                        <button>Log In</button>
+                        <p>{submitResult}</p>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+}
+
+// try removing the onChange prop and typing in a field
+export default LoginForm;
