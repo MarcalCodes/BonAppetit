b4b36e99ab50f888b74bc20f13393136fbacf571
Ingrid Marcal <marcal.delaine@gmail.com>
2025-03-31 19:06:50 +1100

    add UserProvider and LoginForm (#7)
    * add UserProvider and LoginForm
    
    * Show login form when liked a recipe
    
    * save the recipe in a global app state
    
    * Implement favourites behaviour

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -5,34 +5,66 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import {Link, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import {Chip} from "@mui/material";
+import {useUserContext} from "../Context/userContext.jsx";
+import SimpleModal from "./SimpleModal.jsx";
+import {useState} from "react";
+import LoginForm from "./LoginForm.jsx";
 
-const RecipeCard = ({id, name, image, category, area, isFavourite}) => {
+const RecipeCard = ({recipe, isFavourite, addFavourite, removeFavourite}) => {
     const navigate = useNavigate()
+    const {currentUser} = useUserContext()
+    const [favourite, setFavourite] = useState(isFavourite)
+    const [openLoginPopup, setOpenLoginPopup] = useState(false)
+
+    const showLoginPopup = () => {
+        setOpenLoginPopup(true)
+    }
+
+    const handeFavouriteClick = () => {
+        if (!currentUser) {
+            showLoginPopup()
+        } else {
+            const isFavourite = !favourite
+            setFavourite(isFavourite)
+            if (isFavourite) {
+                addFavourite(recipe)
+            } else {
+                removeFavourite(recipe)
+            }
+        }
+    }
 
     return (
-        <Card sx={{ minWidth: 345, maxWidth: 345 }}>
-            <CardMedia
-                component="img"
-                alt="green iguana"
-                height="280"
-                image={image}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {name}
-                </Typography>
-                <Chip label={category} sx={{ mr: 0.5 }} />
-                <Chip label={area} sx={{ mr: 0.5 }}/>
-            </CardContent>
-            <CardActions>
-                <Button size="small">{isFavourite ? <FavoriteIcon /> : <FavoriteBorderIcon /> }</Button>
-                <Button size="small" onClick={() => navigate(`/recipe/${id}`)}>Recipe</Button>
-            </CardActions>
-        </Card>
+        <>
+            <SimpleModal title={"Login"} open={openLoginPopup} setOpen={setOpenLoginPopup}>
+                <LoginForm />
+            </SimpleModal>
+            <Card sx={{minWidth: 345, maxWidth: 345}}>
+                <CardMedia
+                    component="img"
+                    alt="green iguana"
+                    height="280"
+                    image={recipe.strMealThumb}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {recipe.strMeal}
+                    </Typography>
+                    <Chip label={recipe.strCategory} sx={{mr: 0.5}}/>
+                    <Chip label={recipe.strArea} sx={{mr: 0.5}}/>
+                </CardContent>
+                <CardActions>
+                    <Button size="small" onClick={handeFavouriteClick}>
+                        {favourite ? <FavoriteIcon/> : <FavoriteBorderIcon/>}
+                    </Button>
+                    <Button size="small" onClick={() => navigate(`/recipe/${recipe.idMeal}`)}>Recipe</Button>
+                </CardActions>
+            </Card>
+        </>
     );
 }
 
